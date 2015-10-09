382af95800b058e532e257e94d72a5e42b870f9c
Pie227 <meepwill@gmail.com>
2015-10-08 22:08:56 -0700

    Update Renderer.java

--- Renderer.java
+++ Renderer.java
@@ -1,18 +1,18 @@
 package main.game;
 
-import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-@SuppressWarnings("serial")
 public class Renderer extends JPanel {
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		// Background Image Here
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, Broaden.WIDTH, Broaden.HEIGHT);
 	}
+	
+	
+	
 }
